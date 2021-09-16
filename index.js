4e99bbe6d47ea112d95b2a40d22b382f1d15e8fb
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-09-16 20:48:00 +0200

    added delete to my shopping cart

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const loadBooks = () => {
+const loadBooks = (event) => {
   fetch("https://striveschool-api.herokuapp.com/books")
     // WAITING
     .then((response) => response.json())
@@ -14,10 +14,10 @@ const loadBooks = () => {
                        <div class="card">
                             <img src="${element.img}" class="img-fluid h-100 w-100" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">${element.title}</h5>
+                                <h5 id="clamp"  class="card-title">${element.title}</h5>
                                 <p class="card-text">${element.price}€</p>
-                                <a href="#" class="btn btn-primary mr-1">Add to cart</a>
-                                <a href="#" class="btn btn-primary">skip</a>
+                                <a href="#"  class="btn btn-primary mr-1"  onclick= "removeCard(event,'${element.title}','${element.img}')">Add to cart</a>
+                                <a href="#" class="btn btn-primary" style ="background-color:red" onclick="deleteCard(event)">skip</a>
                             </div>
                         </div>
                         `;
@@ -28,13 +28,34 @@ const loadBooks = () => {
     .catch((error) => console.error(error));
 };
 
-function addCart() {}
+function addCart() {
+  removeCard;
+}
 
 let newArray = [];
-function removeCard(event) {
-  let card = event.target.value;
-  card.remove();
+
+function removeCard(event, title, image) {
+  let card = event.target.closest(".col-md-3");
+
   newArray.push(card);
+  let listNode = document.querySelector(".dropdown-menu");
+  let cartList = document.createElement("a");
+  cartList.className = "list-item";
+  cartList.innerHTML = `<div class="d-flex mb-1">
+  <a class="dropdown-item" href="#" ><span><img src="${image}" alt="" width="25px"></span>${title}<span><a href="#" class="cart-btn btn-primary " onclick="deleteShoppingCart(event)" >delete</a></span></a>
+  </div>
+  `;
+  listNode.appendChild(cartList);
+}
+
+function deleteCard(event) {
+  event.target.closest(".col-md-3").remove();
+}
+
+function deleteShoppingCart(event) {
+  console.log(event.target);
+  let removedCart = event.target.closest(".list-item");
+  removedCart.remove();
 }
 
 window.onload = () => {
